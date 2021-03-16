39437f627cfb1de6127349f86a9d405178005406
Zulfan Kasilas Dinca <zulfandinca64@gmail.com>
2021-03-16 22:13:54 +0700

    rename email in myscript.sh

--- TXT/myscript.sh
+++ TXT/myscript.sh
@@ -16,7 +16,7 @@
 # You new to set "REC2" with your own Public-Key Identity!
 # Check it out with "gpg --list-key"
 
-REC2="cbk@dummy"
+REC2="zulfandinca64@gmail.com"
 REC1="operatingsystems@vlsm.org"
 FILES="my*.asc my*.txt my*.sh"
 SHA="SHA256SUM"
